855ef157516f3738f3d99d1774500fe1258f66c9
Kav1shka <103574964+Kav1shka@users.noreply.github.com>
2023-02-18 19:41:10 +0530

    Register implemented

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const users =require("./routes/users.js");
-const passport = require("passport");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const users =require("./Routes/AuthRoute");
 
-const dotenv=require("dotenv");
+// const passport = require("passport");
+
+const dotenv = require("dotenv");
 dotenv.config();
 
-const app=express(); 
+const app = express();
+mongoose.set("strictQuery", false);
 
-const PORT= process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
-
 
 app.use(bodyParser.json());
 
-const url ="mongodb+srv://Kav1shka:Kavishka123@musicplayer.gow9s1m.mongodb.net/test";
+const url =
+  "mongodb+srv://Kav1shka:Kavishka123@musicplayer.gow9s1m.mongodb.net/test";
 
-mongoose.connect(url,
-    err => {
-        if(err) throw err;
-        console.log('MongoDB is connected...')
-    });
+mongoose.connect(url, (err) => {
+  if (err) throw err;
+  console.log("MongoDB is connected...");
+});
+
+const AauthenticationRouter = require("./Routes/AuthRoute");
+app.use("/customer", AauthenticationRouter);
+
+// app.use("/users", users)
+
+app.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
