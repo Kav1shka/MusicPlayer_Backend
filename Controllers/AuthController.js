1d6630feaa8cd552e36c927b9cb495468b4324d2
Kav1shka <103574964+Kav1shka@users.noreply.github.com>
2023-02-25 19:44:50 +0530

    2023.2.25

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,44 +1,45 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User=require("../Models/user")
-
+const User = require("../Models/user");
 
 const validateRegisterInput = require("../Validation/register");
 // const validateLoginInput = require("../Validation/login");
 
-const AuthController={
-    register:async(req,res)=> {
-        try{
-          const Email=req.body.Email;  
-          const password=req.body.password;  
-          const password2=req.body.password2;  
-          const errorMessage=validateRegisterInput(Email,password,password2);
+const AuthController = {
+  register: async (req, res) => {
+    try {
+      const Email = req.body.Email;
+      const password = req.body.password;
+      const password2 = req.body.password2;
+      console.log("came here 1");
+      const errorMessage = validateRegisterInput(Email, password, password2);
+      console.log("came here 2");
+      if (errorMessage) return res.status(400).json({ message: errorMessage });
 
-          if (errorMessage) return res.status(400).json({ message: errorMessage });
+      const userExists = await User.findOne({ Email });
 
-          const userExists=await User.findOne({Email});
-          
-          if(userExists){
-            return res.status(400).json({message:"This email is already in use"})
-          }
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ message: "This email is already in use" });
+      }
 
-          const hashpassword=await bcrypt.hash(password,10);
+      const hashpassword = await bcrypt.hash(password, 10);
 
-          await new User({
-            Email,
-            password:hashpassword,
-            password2
-          }).save();
-          res.status(201).json({
-            message: "You have successfully registered. Please login now",
-          });
-
-        }catch(error){
-          console.log(error);
-          res.status(500).json({message:error.message});
-        }
+      await new User({
+        Email,
+        password: hashpassword,
+        password2,
+      }).save();
+      res.status(201).json({
+        message: "You have successfully registered. Please login now",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
     }
-}
+  },
+};
 
-module.exports=AuthController;
+module.exports = AuthController;
--- Validation/register.js
+++ Validation/register.js
@@ -1,28 +1,38 @@
 const Validator = require("validator");
-const isEmpty = require("is-empty");
+const isEmpty=require("../is-Empty");
 module.exports = function validateRegisterInput(data) {
-let errors={};
-data.Email=!isEmpty(data.Email)? data.Email:"";
-data.password = !isEmpty(data.password) ? data.password : "";
-data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
+let errors={};
+console.log("came here @");
+data.Email=!isEmpty(data.Email)? data.Email:'';
+console.log("came here #");
+data.password = !isEmpty(data.password) ? data.password :'';
+data.password2 = !isEmpty(data.password2) ?  data.password2 :'';
+
+console.log("came here 3");
 if(Validator.isEmpty(data.Email)){
-    errors="Email field is required!!";
+    errors.Email="Email field is required!!";
 }
+if(Validator.isEmail(data.Email)){
+  errors.Email="This is not valid Email!!";
+}
+console.log("came here 4");
 if (Validator.isEmpty(data.password)) {
-    errors = "Password field is required";
+    errors.password = "Password field is required";
   }
 if (Validator.isEmpty(data.password2)) {
-    errors = "Confirm password field is required";
+    errors.password2 = "Confirm password field is required";
   }
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors = "Password must be at least 6 characters";
+    errors.password = "Password must be at least 6 characters";
   }
 if (!Validator.equals(data.password, data.password2)) {
-    errors = "Passwords must match";
+    errors.password2 = "Passwords must match";
   }
+  console.log("came here 5");
   return {
     errors,
     isValid: isEmpty(errors)
+  
   };
 };
