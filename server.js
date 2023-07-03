9c2a37660a96ec60cad1249677e52b1aea321505
Kav1shka <103574964+Kav1shka@users.noreply.github.com>
2023-07-03 21:59:23 +0530

    2023.7.3

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ dotenv.config();
 const app = express();
 mongoose.set("strictQuery", false);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 app.use(cors());
 
 app.use(bodyParser.json());
